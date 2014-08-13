c02a847093a9a662c397c1d5682add44c2708775
justtestnoe <1823997949@qq.com>
2014-08-13 14:50:21 +0800

    搞定了注册登录模块

--- login.php
+++ login.php
@@ -1,14 +1,29 @@
-<?php 
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php 
+header("Content-Type: text/html; charset=utf-8");
 $username=$_POST['username'];
 $password=$_POST['password'];
-
-
 $con=mysql_connect("localhost","root","");
 if(!$con){
-	die("数据库连接失败")
+	die("数据库连接失败");
 }else{
 	echo "连接成功";
 }
-
-
- ?>
+mysql_select_db("love",$con);
+$select="SELECT * FROM user where username='$username' and password='$password'";
+$result=mysql_query($select,$con);
+if(!mysql_fetch_assoc($result)) {
+	die("用户名或密码错误");
+}else{
+	echo "登录成功";
+}
+?>
+<a href="home.html">点击这里去首页</a>
+</body>
+</html>
--- regist.php
+++ regist.php
@@ -1,15 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
 <?php 
+header("Content-Type: text/html; charset=utf-8");
 $username=$_POST['username'];
 $password=$_POST['password'];
-
-
 $con=mysql_connect("localhost","root","");
 if(!$con){
-	die("数据库连接失败")
+	die("数据库连接失败");
 }else{
 	echo "连接成功";
 }
 mysql_select_db("love",$con);
-$insert="INSERT INTO user (username,password) values($username,$password)";
-
+$insert="INSERT INTO user (username,password) values('$username','$password')";
+$result=mysql_query($insert,$con);
+if (!$result) {
+	die("注册失败");
+}else{
+	echo "注册成功";
+}
  ?>
+<a href="index.html">点击这里去登录</a>
+</body>
+</html>
